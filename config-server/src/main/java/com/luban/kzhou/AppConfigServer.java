79d0678795b99f9a4a7348338fc06e3bf87f497c
kzhou <Kaidong.Zhou@eisgroup.com>
2020-09-17 08:46:34 +0800

    2020年9月17日08:46:05 HystrixDashBoard + Config

---
+++ config-server/src/main/java/com/luban/kzhou/AppConfigServer.java
@@ -0,0 +1,16 @@
+package com.luban.kzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class AppConfigServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfigServer.class);
+    }
+}
--- zuul/src/main/java/com/kzhou/luban/filter/LogFilter.java
+++ zuul/src/main/java/com/kzhou/luban/filter/LogFilter.java
@@ -4,9 +4,11 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Component
 public class LogFilter extends ZuulFilter {
     @Override
     public String filterType() {
